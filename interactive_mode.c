379bcca41440b6405c59592bb6451c0f116fcc5d
Kali <kali@kali>
2023-06-20 17:15:39 -0400

    Ajout des commentaires dans les fichiers

--- interactive_mode.c
+++ interactive_mode.c
@@ -1,47 +1,44 @@
-#include <ncurses.h>
-#include <stdlib.h>
-#include <string.h>
+#include <ncurses.h>    // Inclusion de la bibliothèque ncurses
+#include <stdlib.h>     // Inclusion de la bibliothèque standard C
+#include <string.h>     // Inclusion de la bibliothèque de manipulation de chaînes de caractères en C
 
 void start_interactive_mode() {
-    // Initialiser ncurses
-    initscr();
-    noecho();
-    curs_set(FALSE);
+    initscr();          // Initialisation de l'environnement ncurses
+    noecho();           // Désactivation de l'écho des caractères saisis par l'utilisateur
+    curs_set(FALSE);    // Masquage du curseur sur l'écran
 
-    // Main loop
-    while (1) {
-        clear();
-        printw("Actions: extract file or directory(e), visit directory(v), open file(o), quit(q)\n");
+    while (1) {         // Boucle principale du mode interactif
+        clear();        // Efface l'écran
+        printw("Actions: extract file or directory(e), visit directory(v), open file(o), quit(q)\n");  // Affiche les options disponibles
         printw("1 - some_directory\n");
         printw("2 - some_file.txt\n");
         printw("3 - some_other_file.txt\n");
 
-        char ch = getch();
+        char ch = getch();      // Lecture de la touche pressée par l'utilisateur
 
-        switch(ch) {
-            case 'q':
-                endwin();
-                exit(0);
-            case 'e':
+        switch(ch) {            // Traitement de la touche pressée
+            case 'q':           // Si 'q' est pressé, quitter le programme
+                endwin();       // Terminer l'utilisation de la bibliothèque ncurses
+                exit(0);        // Quitter le programme
+            case 'e':           // Si 'e' est pressé, effectuer l'extraction d'un fichier ou d'un répertoire
                 printw("Extract file or directory\n");
                 // Ajoutez ici le code pour extraire le fichier ou le répertoire
                 break;
-            case 'v':
+            case 'v':           // Si 'v' est pressé, effectuer la visite d'un répertoire
                 printw("Visit directory\n");
                 // Ajoutez ici le code pour visiter le répertoire
                 break;
-            case 'o':
+            case 'o':           // Si 'o' est pressé, ouvrir un fichier
                 printw("Open file\n");
                 // Ajoutez ici le code pour ouvrir le fichier
                 break;
-            default:
+            default:            // Si une autre touche est pressée, afficher un message d'erreur
                 printw("Unknown command: %c\n", ch);
                 break;
         }
-        
-        getch(); // Attendre que l'utilisateur appuie sur une touche avant de rafraîchir l'écran
+
+        getch();    // Attendre que l'utilisateur appuie sur une touche avant de rafraîchir l'écran
     }
 
-    // Terminer ncurses
-    endwin();
+    endwin();   // Terminer l'utilisation de la bibliothèque ncurses
 }
--- main.c
+++ main.c
@@ -3,40 +3,40 @@
 
 int main(int argc, char *argv[]) {
     int opt;
-    while((opt = getopt(argc, argv, "hobdp:e:i:")) != -1) {  
-        switch(opt) {  
+    while((opt = getopt(argc, argv, "hobdp:e:i:")) != -1) {
+        switch(opt) {
             case 'h':
                 printf("Show help here\n");
                 break;
             case 'o':
                 printf("Open zip file\n");
-                // Call open zip function here
+                // Appeler la fonction d'ouverture de fichier zip ici
                 break;
             case 'b':
                 printf("Bruteforce password\n");
-                // Call bruteforce function here
+                // Appeler la fonction de bruteforce ici
                 break;
             case 'd':
                 printf("Dictionary file: %s\n", optarg);
-                // Call bruteforce with dictionary function here
+                // Appeler la fonction de bruteforce avec dictionnaire ici
                 break;
             case 'p':
                 printf("Password: %s\n", optarg);
-                // Call decrypt zip with password function here
+                // Appeler la fonction de déchiffrement de fichier zip avec mot de passe ici
                 break;
             case 'e':
                 printf("Extract file: %s\n", optarg);
-                // Call extract file function here
+                // Appeler la fonction d'extraction de fichier ici
                 break;
             case 'i':
                 printf("Include file: %s\n", optarg);
-                // Call include file function here
+                // Appeler la fonction d'inclusion de fichier ici
                 break;
-            default:  
+            default:
                 printf("Unknown option: %c\n", opt);
-                break;  
-        }  
-    } 
+                break;
+        }
+    }
 
     return 0;
 }
